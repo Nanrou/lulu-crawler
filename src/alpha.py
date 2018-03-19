6d9dc7882eb8c4da512e2f465fd2ed3f154c4d30
Nanrou <kkkcomkkk@qq.com>
2018-03-19 11:32:56 +0800

    fix bug

--- src/alpha.py
+++ src/alpha.py
@@ -8,35 +8,41 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from lxml import etree
 import requests
 
-HEADER = {}
+HEADER = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0'}
 MAX_TRY_AGAIN_TIME = 3
 TIMEOUT = 3
-UrlRule = namedtuple('UrlRule', ['url', 'rule'])  # url是网址，rule中是字典: {内容1: 规则1, ...}
+UrlDetail = namedtuple('UrlDetail', ['url', 'detail'])  # url是网址，detail中是字典: {内容1: 规则1, ...}
 
 
 def fetch():
     pass
 
 
-def transform2utf8(text):
-    pass
+def transform2utf8(resp):
+    if resp.encoding == 'utf-8':
+        return resp.text
+    else:
+        return resp.content.encode('gbk').decode('gbk').encode('utf-8')  #  待验证
 
 
-def scrape_core(url_rule):
+def scrape_core(url_detail):
     max_try_again_time = MAX_TRY_AGAIN_TIME
     while max_try_again_time:
         try:
-            response = requests.get(url_rule.url, headers=HEADER, timeout=TIMEOUT)
+            response = requests.get(url_detail.url, headers=HEADER, timeout=TIMEOUT)
             if response.status_code == 200:
-                text = transform2utf8(response.text)  # 要注意编码
-                html = etree.HTML(text)  #
+                text = transform2utf8(response)  # 要注意编码
+                html = etree.HTML(text)
                 scrape_res = {}
-                for k, v in url_rule.rule.items():
+                for k, v in url_detail.detail.items():
                     try:
-                        scrape_res[k] = html.xpath(v)  # 提取内容
-                    except Exception:  # 后面可以看一下需要捕捉什么异常
-                        pass
-                return UrlRule(url=url_rule.url, rule=scrape_res)
+                        ele = html.xpath(v)[0]  # 提取内容
+                        scrape_res[k] = etree.tostring(ele, encoding='utf-8').decode('utf-8')  # 提取内容
+                    except IndexError:
+                        scrape_res[k] = None
+                    except Exception as exc:  # 后面可以看一下需要捕捉什么异常
+                        print('inside', exc)
+                return UrlDetail(url=url_detail.url, detail=scrape_res)
 
             else:  # 非超时的请求失败，后面可以再看怎么处理
                 max_try_again_time -= 1
@@ -47,10 +53,10 @@ def scrape_core(url_rule):
         raise requests.RequestException  # request部分失败，抛出特定异常给上层处理
 
 
-def scrape(url_rules):
+def scrape(url_details):
     res = []
-    with ThreadPoolExecutor(max_workers=20) as executor:
-        url_mapper = {executor.submit(scrape, url_rule): url_rule for url_rule in url_rules}
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        url_mapper = {executor.submit(scrape_core, url_detail): url_detail for url_detail in url_details}
         for future in as_completed(url_mapper):
             _u = url_mapper[future]
             try:
@@ -58,9 +64,9 @@ def scrape(url_rules):
             except requests.RequestException:
                 print()
             except Exception as exc:
-                print(exc)
+                print('outside', exc)
+    return res  # ['url1': urlDetail(url1, {key: value})]
 
 
 if __name__ == '__main__':
-    test_url = [
-        'http://www.zjswater.gov.cn/ContentSubList.aspx?sortid=2&typeid=2&title=%E6%B0%B4%E5%8A%A1%E8%A6%81%E9%97%BB&subtitle=%E6%B0%B4%E5%8A%A1%E5%8A%A8%E6%80%81']
+    pass
