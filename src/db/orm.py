d23e708881dc7f6613b706ac73b821b9ed148c74
Nanrou <kkkcomkkk@qq.com>
2018-03-21 18:28:37 +0800

    完成三分一

--- src/db/orm.py
+++ src/db/orm.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from peewee import MySQLDatabase, Model, CharField, SmallIntegerField, IntegerField, BooleanField, TimeField, TextField, ForeignKeyField
+from peewee import MySQLDatabase, Model, CharField, SmallIntegerField, IntegerField, BooleanField, TimeField, TextField, \
+    ForeignKeyField
 
 mysql_db = MySQLDatabase('shuiwujia', user='root', password='nanrou',
                          host='127.0.0.1', port=3306)
@@ -10,8 +11,10 @@ class BaseModel(Model):
         database = mysql_db
 
 
+# 这三个表是储存数据的
+
 class CompanyTable(BaseModel):
-    gid = CharField(max_length=32, null=True, unique=True)
+    # gid = CharField(max_length=32, null=True, unique=True)
     domain = CharField(max_length=255, null=False, unique=True, index=True)
     name = CharField(max_length=32, null=False, unique=False)
     status = SmallIntegerField(default=0)
@@ -21,9 +24,20 @@ class CompanyTable(BaseModel):
 
 
 class CategoryTable(BaseModel):
-    gid = CharField(max_length=32, null=True, unique=True)
-    category_name = CharField(max_length=32, null=False)
-    category_url = CharField(max_length=255, null=False, unique=True, index=True)
+    # gid = CharField(max_length=32, null=True, unique=True)
+    name = CharField(max_length=32, null=False)
+    url = CharField(max_length=255, null=False, unique=True, index=True)
+
+    condition = SmallIntegerField(default=0, null=False)
+    direct = BooleanField(default=False)  # 是否可以直接在分类页拿到内容
+    is_json = BooleanField(default=False)  # 内容是否为json格式
+
+    # 内容的解析规则
+    article_url_rule = CharField(max_length=255, null=True)
+    article_title_rule = CharField(max_length=255, null=False)
+    article_author_rule = CharField(max_length=255, null=False)
+    article_publish_time_rule = CharField(max_length=255, null=False)
+    article_content_rule = CharField(max_length=255, null=False)
 
     company_name = ForeignKeyField(CompanyTable, on_delete=True, related_name='name', backref='category')
 
@@ -32,14 +46,13 @@ class CategoryTable(BaseModel):
 
 
 class ArticleTable(BaseModel):
-    gid = CharField(max_length=64, null=True, unique=True)
-
-    article_url = CharField(max_length=255, unique=True)
-    article_title = CharField(max_length=128, null=True, unique=True)
-    article_author = CharField(max_length=32, null=True, unique=True)
-    article_publish_time = TimeField(default=datetime.now())
-    article_collected_time = TimeField(default=datetime.now())
-    article_content = TextField(null=True)
+    # gid = CharField(max_length=64, null=True, unique=True)
+    url = CharField(max_length=255, unique=True)
+    title = CharField(max_length=128, null=True, unique=True)
+    author = CharField(max_length=32, null=True, unique=True)
+    publish_time = TimeField(default=datetime.now())
+    collected_time = TimeField(default=datetime.now())
+    content = TextField(null=True)
 
     company_name = ForeignKeyField(CompanyTable, on_delete=True, related_name='name', backref='article')
     category_name = ForeignKeyField(CategoryTable, on_delete=True, related_name='category_name', backref='article')
--- src/lulu/alpha.py
+++ src/lulu/alpha.py
@@ -1,6 +1,9 @@
 """
 核心模块，接收网址和规则，返回爬取的内容
 
+demo
+爬取每个网站资讯的第一页
+
 细分
 1，每天监控，就是那些所有数据均是ajax的
 2，以往数据，只有下一页是ajax的
@@ -9,9 +12,11 @@
 
 写多个逻辑来做映射，中间写适配
 
-condition 0: 普通的静态页面分析
+针对分类页:
+condition 0: 普通的静态页面解析
 condition 1: 需要用无头浏览器去拿sessionID
-condition 2: 可以通过构造特定网址去get
+condition 2: 构造特定网址去拿到json数据
+一般情况下，逻辑是：分类页 -> 文章页 -> 内容，但是后两种情况，有时候会出现：分类页 -> 内容
 
 ======================================
 
@@ -55,7 +60,7 @@ webform的特点是首页是直接可以get到，后续分页都是post拿
 """
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from urllib.parse import unquote
+from urllib.parse import urlparse, urlunparse
 
 from lxml import etree
 import requests
@@ -84,12 +89,16 @@ LOGGER = MyLogger('scrap-part')
 
 
 class Item:
-    """ 单个任务单位 """
-    __slots__ = ['url', 'detail']
+    """ 分类页的任务单位 """
+    __slots__ = ['url', 'detail', 'direct', 'json']
 
-    def __init__(self, url, detail):
+    def __init__(self, url, detail, direct=False, json=False):
         self.url = url
         self.detail = detail
+        self.direct = direct  # 分类是不是直接到内容
+
+        # TODO 暂时没遇到这种，后面再去补
+        # self.json = json  # 内容是不是json格式
 
 
 class StaticItem(Item):
@@ -101,63 +110,162 @@ class HeadlessItem(Item):
 
 
 class AjaxItem(Item):
-    """ condition 3，普通的ajax，通过索引页拿到相关json数据，然后构造URL去拿内容"""
+    """ condition 2，普通的ajax，通过索引页拿到相关json数据，然后构造URL去拿内容"""
+
+
+class Crawler:
+    def __init__(self, items):
+        self.static_items = []
+        self.headless_items = []
+        self.ajax_items = []
+
+        for item in items:
+            if type(item).__name__.startswith('S'):
+                self.static_items.append(item)
+            elif type(item).__name__.startswith('H'):
+                self.headless_items.append(item)
+            else:
+                self.ajax_items.append(item)
+
+    def crawl(self):
+        return self._scrap_static()
+
+    def _scrap_static(self):
+        res = []
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            url_mapper = {executor.submit(self._scrape_core, url_detail): url_detail.url for url_detail in
+                          self.static_items}
+            for future in as_completed(url_mapper):
+                _u = url_mapper[future]
+                try:
+                    res.append({_u: future.result()})
+                except OutTryException:
+                    LOGGER.warning('{} 超过重试次数'.format(_u))
+                except Exception as exc:
+                    LOGGER.warning(('scrap static outside: ', exc))
+        return res  # ['url1': urlDetail(url1, {key: value})]
+
+    def _scrape_core(self, url_detail):  # todo 计数
+        max_try_again_time = MAX_TRY_AGAIN_TIME
+        while max_try_again_time:
+            try:
+                response = requests.get(url_detail.url, headers=HEADER, timeout=TIMEOUT)
+                if response.status_code == 200:
+                    if url_detail.direct:  # 如果是直接可以获得内容，则数据形式肯定是json
+                        url_detail.detail.pop('article_url_rule')  # 去掉无用规则
+                        return self.fetch_json(response.json(), url_detail)
+                    else:  # 否则就要从当前分类页获取文章页面的url，然后去各页面取
+                        html = etree.HTML(self.transform2utf8(response))  # 要注意编码
+                        # article_urls = html.xpath(url_detail.detail.pop('article_url_rule'))
+                        tt = url_detail.detail.pop('article_url_rule')
+                        article_urls = html.xpath(tt)
+                        try:
+                            o = urlparse(article_urls[0])
+                            if not (o.scheme and o.netloc):  # 如果这部分的url是相对url，则需要拼接起来
+                                _o = urlparse(url_detail.url)
+                                article_urls = [urlunparse([_o.scheme, _o.netloc, _url, '', '', ''])
+                                                for _url in article_urls]
+                                # article_urls = [''.join([url_detail.url, _url]) for _url in article_urls]
+                            with requests.Session() as session:  # 后面改成协程
+                                session.headers.update(HEADER)
+                                res = []
+                                # for article_url in article_urls:
+                                #     try:
+                                #         resp = session.get(article_url, timeout=TIMEOUT)
+                                #         text = self.transform2utf8(resp)  # 要注意编码
+                                #         url_detail.url = article_url  # 更新成当前页面的网址
+                                #         res.append(self.fetch(text, url_detail))
+                                #     except requests.Timeout:
+                                #         LOGGER.warning('超时 {}'.format(article_url))
+                                url_details = [UrlDetail(url, url_detail.detail) for url in article_urls][:5]
+                                with ThreadPoolExecutor(max_workers=10) as executor:
+                                    url_mapper = {executor.submit(self.fetch_thread, session, url_detail): url_detail
+                                                  for url_detail in url_details}
+                                    for future in as_completed(url_mapper):
+                                        _u = url_mapper[future]
+                                        try:
+                                            res.append(future.result())
+                                        except Exception as exc:
+                                            LOGGER.warning(('scrap static normal: ', _u.detail, exc))
+                            return res
+                        except IndexError:
+                            LOGGER.warning('{} 的 article url rule 部分有问题'.format(url_detail.url))
+                else:  # 非超时的请求失败，后面可以再看怎么处理
+                    max_try_again_time -= 1
+                    continue
+            except requests.Timeout:
+                max_try_again_time -= 1
+        else:
+            raise OutTryException  # request部分失败，抛出特定异常给上层处理
+
+    @staticmethod
+    def fetch_json(json, url_detail):  # 返回的是所有文章的UrlDetail列表
+        if not isinstance(json, list):  # 适配单个的情况
+            json = [json]
+        res = []
+        for obj in json:
+            scrape_res = {}
+            for k, v in url_detail.detail.items():
+                try:
+                    scrape_res[k] = eval('obj{}'.format(v))
+                except KeyError:
+                    scrape_res[k] = None
+                    LOGGER.warning('{} 的 {} 部分规则有问题'.format(url_detail.url, k))
+                except Exception as exc:  # 后面可以看一下需要捕捉什么异常
+                    LOGGER.warning('In fetch json: ', exc)
+            res.append(UrlDetail(url=url_detail.url, detail=scrape_res))
+        return res
+
+    @staticmethod
+    def fetch(text, url_detail):  # 返回的是每一个文章的UrlDetail
+        html = etree.HTML(text)
+        scrape_res = {}
+        for k, v in url_detail.detail.items():
+            try:
+                ele = html.xpath(v)[0]
+                # scrape_res[k] = etree.tostring(ele, encoding='utf-8').decode('utf-8')  # 提取内容
+                scrape_res[k] = etree.tostring(ele, encoding='utf-8').decode('utf-8')  # 提取内容
+            except IndexError:
+                scrape_res[k] = None
+                LOGGER.warning('{} 的 {} 部分规则有问题'.format(url_detail.url, k))
+            except Exception as exc:  # 后面可以看一下需要捕捉什么异常
+                LOGGER.warning('In fetch: ', exc)
+        return UrlDetail(url=url_detail.url, detail=scrape_res)
+
+    # 文章的线程函数
+    def fetch_thread(self, session, url_detail):
+        try:
+            resp = session.get(url_detail.url, timeout=TIMEOUT)
+            html = etree.HTML(self.transform2utf8(resp))
+            scrape_res = {}
+            for k, v in url_detail.detail.items():
+                try:
+                    ele = html.xpath(v)[0]
+                    # scrape_res[k] = etree.tostring(ele, encoding='utf-8').decode('utf-8')  # 提取内容
+                    scrape_res[k] = etree.tostring(ele, encoding='utf-8').decode('utf-8')  # 提取内容
+                except IndexError:
+                    scrape_res[k] = None
+                    LOGGER.warning('{} 的 {} 部分规则有问题'.format(url_detail.url, k))
+                except Exception as exc:  # 后面可以看一下需要捕捉什么异常
+                    LOGGER.warning('In fetch: ', exc)
+            return UrlDetail(url=url_detail.url, detail=scrape_res)
+
+        except requests.Timeout:
+            LOGGER.warning('超时 {}'.format(url_detail.url))
+
+    @staticmethod
+    def transform2utf8(resp):
+        if resp.encoding == 'utf-8':
+            return resp.text
+        else:
+            return resp.content.encode('gbk').decode('gbk').encode('utf-8')  # 待验证
+
+    def _scrap_headless(self):
+        pass
 
 
 def fetch():
     pass
-
-
-def transform2utf8(resp):
-    if resp.encoding == 'utf-8':
-        return resp.text
-    else:
-        return resp.content.encode('gbk').decode('gbk').encode('utf-8')  # 待验证
-
-
-def scrape_core(url_detail):
-    max_try_again_time = MAX_TRY_AGAIN_TIME
-    while max_try_again_time:
-        try:
-            response = requests.get(url_detail.url, headers=HEADER, timeout=TIMEOUT)
-            if response.status_code == 200:
-                text = transform2utf8(response)  # 要注意编码
-                html = etree.HTML(text)
-                scrape_res = {}
-                for k, v in url_detail.detail.items():
-                    try:
-                        ele = html.xpath(v)[0]
-                        # scrape_res[k] = etree.tostring(ele, encoding='utf-8').decode('utf-8')  # 提取内容
-                        scrape_res[k] = etree.tostring(ele, encoding='utf-8').decode('utf-8')  # 提取内容
-                    except IndexError:
-                        scrape_res[k] = None
-                        LOGGER.warning('{} 的 {} 部分规则有问题'.format(url_detail.url, k))
-                    except Exception as exc:  # 后面可以看一下需要捕捉什么异常
-                        LOGGER.warning('inside', exc)
-                return UrlDetail(url=url_detail.url, detail=scrape_res)
-
-            else:  # 非超时的请求失败，后面可以再看怎么处理
-                max_try_again_time -= 1
-                continue
-        except requests.Timeout:
-            max_try_again_time -= 1
-    else:
-        raise OutTryException  # request部分失败，抛出特定异常给上层处理
-
-
-def scrape(url_details):
-    res = []
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        url_mapper = {executor.submit(scrape_core, url_detail): url_detail for url_detail in url_details}
-        for future in as_completed(url_mapper):
-            _u = url_mapper[future]
-            try:
-                res.append({_u.url: future.result()})
-            except OutTryException:
-                LOGGER.warning('{} 超过重试次数'.format(_u.url))
-            except Exception as exc:
-                print('outside', exc)
-    return res  # ['url1': urlDetail(url1, {key: value})]
 
 
 def ajax_core(url_detail):
@@ -170,15 +278,46 @@ def scrape_ajax(url_detail):
 
 if __name__ == '__main__':
     rr = '''
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding: gzip, deflate
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Cache-Control: no-cache
+Connection: keep-alive
+Host: www.yiwuwater.com
+Pragma: no-cache
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0
 '''
 
     # pp = {'http': '127.0.0.1:8877'}
-    # rr = requests.post(
-    #     'http://admin.ahwsa.com/php/index.php?a=list&page=1&type=1&pageSize=5',
-    # 'http://www.bdc.cn/biz200/mh204XwdtAction!getNewsContent.act?viewObj.newsId=8a82804d6187a83f0162037a0498130d',
-    # headers=HEADER,
-    # headers=generate_header(rr),
-    # proxies=pp)
+    # rr = requests.get(
+    #     'http://www.yiwuwater.com/api/wap/v1/contentapi/content/getall/true/22',
+    #     headers=generate_header(rr),
+    #     proxies=pp,
+    # )
     # print(rr.json())
-    uu = UrlDetail('ss', 'bb')
-    print(type(uu).__name__)
+    rule1 = {
+        # 'article_content_rule': "['T_BodyContent']",
+        'article_publish_time_rule': "['D_PublishTime']",
+        # 'article_author_rule': "['S_SecondTitle']",
+        'article_title_rule': "['S_SubTitle']",
+        'article_url_rule': None,
+    }
+    u1 = 'http://www.yiwuwater.com/api/wap/v1/contentapi/content/getall/true/22'
+    ss1 = StaticItem(u1, rule1, True)
+
+    rule2 = {
+        # 'article_content_rule': "['T_BodyContent']",
+        'article_publish_time_rule': "//h6[@id='h6Other']",
+        # 'article_author_rule': "['S_SecondTitle']",
+        'article_title_rule': "//h1[@id='h1Other']",
+        'article_url_rule': '//table[@id="gv"]//tr/td/a/@href',
+    }
+    u2 = 'http://www.zjswater.gov.cn/ContentSubList.aspx?sortid=2&typeid=2&title=%E6%B0%B4%E5%8A%A1%E8%A6%81%E9%97%BB&subtitle=%E6%B0%B4%E5%8A%A1%E5%8A%A8%E6%80%81'
+    ss2 = StaticItem(u2, rule2, False)
+
+    cc = Crawler([ss1, ss2])
+    rr = cc.crawl()
+    print(len(rr))
+    for c in rr:
+        print(c)
